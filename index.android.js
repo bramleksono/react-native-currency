5728bd967e491500a1230f68e71820ab7eaa603b
andrinurachman <andrinurachman@gmail.com>
2016-08-04 13:27:40 +0700

    andri

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ class currency extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+        ~ Test react-native currency ~
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
